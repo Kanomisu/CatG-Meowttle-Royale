074ee077f2f074de33ab387e3a56f8cef0586121
Ricardo <ricardojp2511@gmail.com>
2020-10-20 15:31:48 -0400

    Started the very basic framework
    Will assign each of your tasks

--- CATGBATTLEROYALE/CATGBATTLEROYALE.cpp
+++ CATGBATTLEROYALE/CATGBATTLEROYALE.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+using namespace std;
+/*
+This is our game for our firts GDW project!
+CATG Battle royale s textbased and in one file
+Date: 10/20/2020
+Authors: Ricardo Prato, Blahblahblah
+
+*/
+
+void displayIntro();
+void playGame();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	displayIntro();
+	playGame();
+
 }
 
+void displayIntro() {
+
+}
+
+void playGame() {
+	int playerPoints[5];
+	bool gameEnd = false;
+
+	while(!gameEnd)
 
 
+
+
+
+
+}
